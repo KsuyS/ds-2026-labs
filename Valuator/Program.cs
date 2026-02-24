9bd2ae9d2f8ad4645a6205685460587da7674fc6
Ксения Соловьева <ksenia220403@mail.ru>
2026-02-24 21:41:40 +0300

    Сделала 1 лабу

--- Valuator/Program.cs
+++ Valuator/Program.cs
@@ -6,12 +6,15 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddRazorPages();
+
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = "localhost:6379";
+        });
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
