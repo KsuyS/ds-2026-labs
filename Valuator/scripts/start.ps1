c6b6f0c0201d2bb761fa74b3be48cb5cbdbb7a80
Ксения Соловьева <ksenia220403@mail.ru>
2026-03-24 18:06:49 +0300

    Сделала 2-ю лабу

---
+++ Valuator/scripts/start.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Valuator 5001..."
+Start-Process powershell -ArgumentList '-NoExit -Command "cd E:\RP\DISTRIBUTED-PROGRAMMING\Valuator; dotnet run --urls http://0.0.0.0:5001"'
+
+Write-Host "Valuator 5002..."
+Start-Process powershell -ArgumentList '-NoExit -Command "cd E:\RP\DISTRIBUTED-PROGRAMMING\Valuator; dotnet run --urls http://0.0.0.0:5002"'
+
+Start-Sleep 5
+
+Write-Host "Nginx..."
+Push-Location "C:\nginx-1.29.5"
+& .\nginx.exe -c "E:\RP\DISTRIBUTED-PROGRAMMING\nginx\conf\nginx.conf"
+Pop-Location
